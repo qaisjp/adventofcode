a6fbae2c2defeff8743dee30885564b0db2b897b
Qais Patankar <qaisjp@gmail.com>
2020-12-01 15:32:14 +0000

    2020/day1

---
+++ 2020/cmd/day1/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/qaisjp/adventofcode/2020/internal/parse"
+)
+
+var input1 = []int{
+	1721,
+	979,
+	366,
+	299,
+	675,
+	1456,
+}
+
+func main() {
+	puzzleOne := os.Args[1] == "1"
+	if !puzzleOne && os.Args[1] != "2" {
+		panic("Expected first arg to be '1' or '2'")
+	}
+
+	if puzzleOne {
+		firstPuzzle()
+	} else {
+		secondPuzzle()
+	}
+}
+
+func firstPuzzle() {
+	filename := os.Args[2]
+	input := parse.FilenameToInts(filename)
+	for _, a := range input {
+		for _, b := range input {
+			if a+b == 2020 {
+				fmt.Printf("a: %d, b: %d, a + b: %d, a * b: %d\n", a, b, a+b, a*b)
+			}
+		}
+	}
+}
+
+func secondPuzzle() {
+	filename := os.Args[2]
+	input := parse.FilenameToInts(filename)
+	for _, a := range input {
+		for _, b := range input {
+			for _, c := range input {
+				if a+b+c == 2020 {
+					fmt.Printf("a: %d, b: %d, c: %d, a + b + c: %d, a * b * c: %d\n", a, b, c, a+b+c, a*b*c)
+				}
+			}
+		}
+	}
+}
---
+++ 2020/internal/parse/lines.go
@@ -0,0 +1,35 @@
+package parse
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+)
+
+func ForFilenameLines(path string, mapper func(string)) {
+	file, err := os.Open(path)
+	if err != nil {
+		panic(err.Error())
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		mapper(scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		panic(err.Error())
+	}
+}
+
+func FilenameToInts(path string) (results []int) {
+	ForFilenameLines(path, func(line string) {
+		i, err := strconv.Atoi(line)
+		if err != nil {
+			panic(err.Error())
+		}
+		results = append(results, i)
+	})
+	return
+}
