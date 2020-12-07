d3b8a629477edb0695c8f54d3bd2cc14747f8df5
Qais Patankar <qaisjp@gmail.com>
2020-12-07 08:37:43 +0000

    Update templalate

--- 2020/dayXX/main.rb
+++ 2020/dayXX/main.rb
@@ -1,27 +1,28 @@
+# frozen_string_literal: true
+
+# AoC
 class AoC
-	def initialize(data)
-		@data = data
-	end
+  def initialize(data)
+    @data = data
+  end
 
-	def one
-		@data.each do |x|
-			puts "X: #{x}"
-		end
-	end
+  def one
+    @data.each do |x|
+      puts "X: #{x}"
+    end
+  end
 
-	def two
-
-	end
+  def two; end
 end
 
 def main
-	n = ARGV.shift
-	runner = AoC.new ARGF.readlines.to_a
+  n = ARGV.shift
+  runner = AoC.new ARGF.readlines.to_a
 
-	if n == "1"
-		runner.one
-	else
-		runner.two
-	end
+  if n == '1'
+    runner.one
+  else
+    runner.two
+  end
 end
 main
