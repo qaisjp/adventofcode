51377554c5ec40ff2bada4b3dc5e3f77b8f76097
Qais Patankar <qaisjp@gmail.com>
2020-12-08 10:52:21 +0000

    2020/day08/part2

--- 2020/day08/main.rb
+++ 2020/day08/main.rb
@@ -16,7 +16,17 @@ end
 class Instruction < T::Struct
   prop :op, Op
   prop :arg, Integer
+
+  def inspect
+    "Instr(#{op.serialize}, #{arg})"
+  end
+
+  def to_s
+    inspect
+  end
 end
+
+Instructions = T.type_alias {T::Array[Instruction]}
 
 class ProgramExecutor
   extend T::Sig
@@ -29,9 +39,9 @@ class ProgramExecutor
     @pc = 0
   end
 
-  sig {params(instrs: T::Array[Instruction]).void}
+  sig {params(instrs: Instructions).void}
   def initialize(instrs)
-    @instrs = T.let(instrs.dup, T::Array[Instruction])
+    @instrs = T.let(instrs.dup, Instructions)
     @acc = T.let(0, Integer)
     @pc = T.let(0, Integer)
   end
@@ -64,31 +74,71 @@ class ProgramExecutor
 
       @pc = next_pc
     end
+    puts "Execution completed. Acc is #{@acc}"
   end
 end
 
 class AoC
+  extend T::Sig
+
   def initialize(data)
     instrs = data.map do |line|
       op, arg = line.scanf("%s %d")
       Instruction.new(op: Op.deserialize(op), arg: arg)
     end
-    @instructions = T.let(instrs, T::Array[Instruction])
-    @seen_instructions = T.let(Set.new, T::Set[Integer])
+    @instructions = T.let(instrs, Instructions)
   end
 
   def one
     exec = ProgramExecutor.new(@instructions)
-
+    seen_instructions = T.let(Set.new, T::Set[Integer])
     before_exec = lambda do |pc, instr|
-      if @seen_instructions.include?(pc)
+      if seen_instructions.include?(pc)
         puts "Running ##{pc} a second time. Accumulator is #{exec.acc}"
         return false
       end
-      @seen_instructions << pc
+      seen_instructions << pc
       true
     end
     exec.run!(before_exec: before_exec)
+  end
+
+  sig {params(instrs: Instructions, i: Integer).returns(Instructions)}
+  def swap_param(instrs, i)
+    instrs = instrs.dup
+    instr = instrs.fetch(i).dup
+    if instr.op == Op::JMP
+      instr.op = Op::NOP
+    elsif instr.op == Op::NOP
+      instr.op = Op::JMP
+    end
+    instrs[i] = instr
+    instrs
+  end
+
+  def two;
+      index_to_modify = 0
+      max_index = @instructions.size
+      while index_to_modify < max_index
+        new_instructions = swap_param(@instructions, index_to_modify)
+        exec = ProgramExecutor.new(new_instructions)
+
+        success = T.let(true, T::Boolean)
+        seen_instructions = T.let(Set.new, T::Set[Integer])
+        before_exec = lambda do |pc, instr|
+          if seen_instructions.include?(pc)
+            puts "Running ##{pc} a second time. Accumulator is #{exec.acc}"
+            success = false
+            return false
+          end
+          seen_instructions << pc
+          true
+        end
+        exec.run!(before_exec: before_exec)
+
+        break if success
+        index_to_modify += 1
+      end
   end
 end
 
