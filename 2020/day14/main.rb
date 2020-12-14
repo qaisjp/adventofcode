1b7b8846bf3607ff9326a0c3068ee02f8f344d72
Qais Patankar <qaisjp@gmail.com>
2020-12-14 05:44:27 +0000

    opt

--- 2020/day14/main.rb
+++ 2020/day14/main.rb
@@ -13,7 +13,7 @@ class AoC
     # @data = T.let(data.map(&:to_i), T::Array[Integer])
     @data = data
 
-    @mem = T::Array[Integer].new
+    @mem = {}
     @combis = []
   end
 
@@ -42,9 +42,7 @@ class AoC
     # @assigns.each
   end
 
-  def get_addrs(addr, mask_indices)
-    @combis[mask_indices.size] ||= [1, 0].repeated_permutation(mask_indices.size).to_a
-    index_to_vals = @combis[mask_indices.size]
+  def get_addrs(addr, mask_indices, index_to_vals)
     index_to_vals.map do |bit_vals|
       m = addr.dup
       mask_indices.each_with_index do |str_index, i|
@@ -59,24 +57,26 @@ class AoC
     mask = ""
     or_mask = 0
     random_bit_indices = []
+    index_to_vals = []
 
     @data.each do |line|
       if line.start_with? "mask = "
         mask = T.let(line.split("= ")[1], String)
         or_mask = mask.gsub("X", "0").to_i(base=2)
         random_bit_indices = mask.enum_for(:scan, /(?=X)/).map { Regexp.last_match.offset(0).first }
+        index_to_vals = [1, 0].repeated_permutation(random_bit_indices.size).to_a
         next
       end
 
       addr, val = line.scanf("mem[%d] = %d")
       addr |= or_mask
-      get_addrs(addr.to_s(base=2).rjust(36, '0'), random_bit_indices).each do |mod_addr|
+      get_addrs(addr.to_s(base=2).rjust(36, '0'), random_bit_indices, index_to_vals).each do |mod_addr|
         @mem[mod_addr.to_i(base=2)] = val
       end
 
     end
 
-    @mem.reject {|x| x.nil?}.sum
+    @mem.values.sum
 
     # @assigns.each
   end
