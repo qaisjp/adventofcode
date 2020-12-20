1c1ccd09c7fbfc959a315add3ee3d49818b6b41f
Qais Patankar <qaisjp@gmail.com>
2020-12-20 06:19:31 +0000

    broken hsit

--- 2020/day20/main.rb
+++ 2020/day20/main.rb
@@ -15,29 +15,40 @@ class String
 end
 
 class Array
+  def set_rotate_context(n)
+    @context = 4.times.to_a.rotate(-n)
+  end
   def top
-    self[0]
+    i = @context[0]
+    self[i]
   end
   def right
-    self[1]
+    i = @context[1]
+    self[i]
   end
   def bottom
-    self[2]
+    i = @context[2]
+    self[i]
   end
   def left
-    self[3]
+    i = @context[3]
+    self[i]
   end
   def top=(v)
-    self[0] = v
+    i = @context[0]
+    self[i] = v
   end
   def right=(v)
-    self[1] = v
+    i = @context[1]
+    self[i] = v
   end
   def bottom=(v)
-    self[2] = v
+    i = @context[2]
+    self[i] = v
   end
   def left=(v)
-    self[3] = v
+    i = @context[3]
+    self[i] = v
   end
 end
 
@@ -71,6 +82,8 @@ class AoC
       left = rows.map(&:first).join("")
 
       @tile_links[id] = [nil, nil, nil, nil]
+      @tile_links[id].set_rotate_context(0)
+
       @tile_ops[id] = []
 
       [id, [top, right, bottom, left]]
@@ -83,82 +96,85 @@ class AoC
       links = @tile_links[tile_id]
       next if links.compact == 4
 
-      t, r, b, l = edge_strs
+      4.times do |rotate_index|
+        t, r, b, l = edge_strs.rotate(rotate_index)
+        links.set_rotate_context(rotate_index)
 
-      @tile_edges.each do |other_id, other_strs|
-        next if other_id == tile_id
+        @tile_edges.each do |other_id, other_strs|
+          next if other_id == tile_id
 
-        other_links = @tile_links[other_id]
-        next if other_links.compact == 4
+          other_links = @tile_links[other_id]
+          next if other_links.compact == 4
 
-        to, ro, bo, lo = other_strs
+          to, ro, bo, lo = other_strs
 
-        postop = nil
+          postop = nil
 
-        # our top is their bottom
-        # our right is their left
-        # our bottom is their top
-        # our left is their right
-        if t == bo
-          links.top = other_id
-          other_links.bottom = tile_id
-        elsif r == lo
-          links.right = other_id
-          other_links.left = tile_id
-        elsif b == to
-          links.bottom = other_id
-          other_links.top = tile_id
-        elsif l == ro
-          links.left = other_id
-          other_links.right = tile_id
+          # our top is their bottom
+          # our right is their left
+          # our bottom is their top
+          # our left is their right
+          if t == bo
+            links.top = other_id
+            other_links.bottom = tile_id
+          elsif r == lo
+            links.right = other_id
+            other_links.left = tile_id
+          elsif b == to
+            links.bottom = other_id
+            other_links.top = tile_id
+          elsif l == ro
+            links.left = other_id
+            other_links.right = tile_id
 
-        # when you flip this one vertically,
-        # - your top matches with another top
-        # - your right.reverse matches with another left
-        # - your bottom matches with another bottom
-        # - your left.reverse matches with another right
-        elsif t == to
-          postop = 'vertical'
-          links.top = other_id
-          other_links.top = tile_id
-        elsif r.reverse == lo
-          postop = 'vertical'
-          links.right = other_id
-          other_links.left = tile_id
-        elsif b == bo
-          postop = 'vertical'
-          links.bottom = other_id
-          other_links.bottom = tile_id
-        elsif l.reverse == ro
-          postop = 'vertical'
-          links.left = other_id
-          other_links.right = tile_id
+          # when you flip this one vertically,
+          # - your top matches with another top
+          # - your right.reverse matches with another left
+          # - your bottom matches with another bottom
+          # - your left.reverse matches with another right
+          elsif t == to
+            postop = 'vertical'
+            links.top = other_id
+            other_links.top = tile_id
+          elsif r.reverse == lo
+            postop = 'vertical'
+            links.right = other_id
+            other_links.left = tile_id
+          elsif b == bo
+            postop = 'vertical'
+            links.bottom = other_id
+            other_links.bottom = tile_id
+          elsif l.reverse == ro
+            postop = 'vertical'
+            links.left = other_id
+            other_links.right = tile_id
 
-        # when you flip horizontally,
-        # - your top.reverse matches with another bottom
-        # - your right matches with another right
-        # - your bottom.reverse matches with another top
-        # - your left matches with another left
-        elsif t.reverse == bo
-          postop = 'horizontal'
-          links.top = other_id
-          other_links.bottom = tile_id
-        elsif r == ro
-          postop = 'horizontal'
-          links.right = other_id
-          other_links.right = tile_id
-        elsif b.reverse == to
-          postop = 'horizontal'
-          links.bottom = other_id
-          other_links.top = tile_id
-        elsif l == lo
-          postop = 'horizontal'
-          links.left = other_id
-          other_links.left = tile_id
-        end
+          # when you flip horizontally,
+          # - your top.reverse matches with another bottom
+          # - your right matches with another right
+          # - your bottom.reverse matches with another top
+          # - your left matches with another left
+          elsif t.reverse == bo
+            postop = 'horizontal'
+            links.top = other_id
+            other_links.bottom = tile_id
+          elsif r == ro
+            postop = 'horizontal'
+            links.right = other_id
+            other_links.right = tile_id
+          elsif b.reverse == to
+            postop = 'horizontal'
+            links.bottom = other_id
+            other_links.top = tile_id
+          elsif l == lo
+            postop = 'horizontal'
+            links.left = other_id
+            other_links.left = tile_id
+          end
 
-        if postop
-          @tile_ops[tile_id] << postop
+          if postop
+            @tile_ops[tile_id] << postop
+          end
         end
       end
     end
