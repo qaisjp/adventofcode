c1ea0e29dc1b254daf583e366d57f7d29d4f14cb
Qais Patankar <qaisjp@gmail.com>
2020-12-20 21:55:11 +0000

    part1 done

--- 2020/day20/main.rb
+++ 2020/day20/main.rb
@@ -85,6 +85,20 @@ class AoC
     @min_y = 0
     @max_x = 0
     @max_y = 0
+
+    @pos_to_tile = {}
+    @tiles_to_pos = {}
+
+    @directions = 4.times.to_a
+    @opp_directions = @directions.rotate(2)
+    @direction_words = ["top   ", "right ", "bottom", "left  "]
+    @opp_direction_words = @direction_words.rotate(2)
+  end
+
+  def dir_word(dir, opp: false, wide: false)
+    word = (opp ? @opp_direction_words : @direction_words)[dir]
+    word = word.strip unless wide
+    word
   end
 
   def pos_direction(pos, direction)
@@ -101,11 +115,75 @@ class AoC
   end
 
   def set_tile_pos(tile, pos)
-    @tile_pos[pos] = tile
+    @pos_to_tile[pos] = tile
+    @tiles_to_pos[tile] = pos
     @min_x = [@min_x, pos.x].min
     @min_y = [@min_y, pos.y].min
     @max_x = [@max_x, pos.x].max
     @max_y = [@max_y, pos.y].max
+  end
+
+  def build_tile_edges!
+    @tile_edges = @tile_rows.map do |id, rows|
+      top = rows[0]
+      right = rows.map(&:last).join("")
+      bottom = rows[-1]
+      left = rows.map(&:first).join("")
+      [id, [top, right, bottom, left]]
+    end.to_h
+  end
+
+  def does_tile_fit(our_id:, our_edge:, their_id:, their_direction:)
+    # Try all rotations
+    4.times do |rotate_index|
+      # Try all pairs of mirroring
+      ["none", "vert", "horz"].repeated_combination(2).each do |flip1, flip2|
+
+        other_edges = @tile_edges[their_id].rotate(rotate_index).flip_both(flip1, flip2)
+        other_edges2 = @tile_edges[their_id].flip_both(flip1, flip2).rotate(rotate_index)
+
+        # if tile_id == 1427 && their_id == 1489
+        #   puts "rot(#{rotate_index}), flip(#{flip1}, #{flip2}): #{their_word} of #{their_id} is #{other_edges[their_direction]} or #{other_edges2[their_direction]}"
+        # end
+
+        # our top is their bottom
+        # our right is their left
+        # our bottom is their top
+        # our left is their right
+        if our_edge == other_edges[their_direction]
+          puts "FOUND1: rot(#{rotate_index}), flip(#{flip1}, #{flip2})"
+          puts "#{their_id} is placed #{dir_word(their_direction, opp: true)} to #{our_id}"
+          @tile_edges[their_id] = other_edges
+          return true
+        elsif our_edge == other_edges2[their_direction]
+          puts "FOUND2: flip(#{flip1}, #{flip2}), rot(#{rotate_index})"
+          @tile_edges[their_id] = other_edges2
+          puts "#{their_id} is placed #{dir_word(their_direction, opp: true)} to #{our_id}"
+          return true
+        end
+      end
+    end
+
+    return false
+  end
+
+  def bruteforce_find(our_id:, direction:)
+    puts "\n## Looking #{dir_word(direction)} of #{our_id}\n\n"
+    our_edge = @tile_edges[our_id][direction]
+    their_direction = @opp_directions[direction]
+
+    @tiles_to_search.each do |their_id|
+      fits = does_tile_fit(
+        our_id: our_id,
+        our_edge: our_edge,
+        their_id: their_id,
+        their_direction: their_direction
+      )
+      if fits
+        return their_id
+      end
+    end
+    return nil
   end
 
   def one
@@ -113,122 +191,53 @@ class AoC
     edge_width = Math.sqrt(tile_count)
     puts "There are #{tile_count} tiles, edge_width is #{edge_width}"
 
-    @tile_pos = {}
-    # @tile_links = {}
     @tile_ops = {}
-    @free_tiles = []
-    @tile_edges = {}
-
-    @tile_rows.each do |id, rows|
-      top = rows[0]
-      right = rows.map(&:last).join("")
-      bottom = rows[-1]
-      left = rows.map(&:first).join("")
-      @tile_edges[id] = [top, right, bottom, left]
-
-      if id == 1489
-        puts "Edges of 1489 are #{@tile_edges[id]}"
-      end
-
-      # @tile_links[id] = [nil, nil, nil, nil]
+    @tiles_to_search = []
+    build_tile_edges!
+    @tile_rows.map do |id, _|
+      @tiles_to_search << id
       @tile_ops[id] = []
-      @free_tiles << id
     end
-
-    opposite_direction = [0, 1, 2, 3].rotate(2)
-    direction_words = ["top___", "right_", "bottom", "left__"]
 
     pos = 0 + 0i
-    set_tile_pos(@free_tiles.shift, pos)
-    direction = 0
+    set_tile_pos(@tiles_to_search.shift, pos)
 
-    # modified
-    movement_directions = [0, 1, 2, 3]
-    directions_lost = 0
+    iters = 0
+    @positions_to_process = [0 + 0i]
+    while !@positions_to_process.empty?
+      # break if iters == 2
+      iters += 1
 
-    found = true
-    while found do
-      found = nil
-      loopback_skip = false
+      pos = @positions_to_process.shift
+      tile_id = @pos_to_tile[pos]
+      puts "\n############################\n# Currently #{tile_id} is at #{pos}\n############################"
 
-      tile_id = @tile_pos[pos]
-      our_edge = @tile_edges[tile_id][direction]
-      our_word = direction_words[direction]
-
-      their_direction = opposite_direction[direction]
-      their_word = direction_words.rotate(2)[their_direction]
-      next_pos = pos_direction(pos, direction)
-
-      if (tile = @tile_pos[next_pos])
-        puts "\nLoopback on #{next_pos} which is #{tile}, hopping"
-        # loopback_skip = true
-        pos = next_pos
-        found = true
-        next
-      end
-
-      puts "\n\n"
-      puts "#{tile_id} is at #{pos}, trying to place at #{next_pos}"
-      puts "Looking for tiles on #{direction_words[direction]} of #{tile_id}"
-      puts "Findin tile whose #{their_word} matches our #{our_word} #{our_edge} () #{our_edge}"
-
-      @free_tiles.each do |their_id|
-        break if found
-        4.times do |rotate_index|
-          break if found
-          ["none", "vert", "horz"].repeated_combination(2).each do |flip1, flip2|
-            break if found
-
-            other_edges = @tile_edges[their_id].rotate(rotate_index).flip_both(flip1, flip2)
-            other_edges2 = @tile_edges[their_id].flip_both(flip1, flip2).rotate(rotate_index)
-
-            if tile_id == 1427 && their_id == 1489
-              puts "rot(#{rotate_index}), flip(#{flip1}, #{flip2}): #{their_word} of #{their_id} is #{other_edges[their_direction]} or #{other_edges2[their_direction]}"
-            end
-            # our top is their bottom
-            # our right is their left
-            # our bottom is their top
-            # our left is their right
-            if our_edge == other_edges[their_direction]
-              puts "FOUND1: rot(#{rotate_index}, flip(#{flip1}, #{flip2})"
-              puts "#{their_id} is placed next to #{tile_id}"
-              @tile_edges[their_id] = other_edges  # this should be here but it breaks things
-              found = their_id
-            elsif our_edge == other_edges2[their_direction]
-              puts "FOUND2: flip(#{flip1}, #{flip2}), rot(#{rotate_index}"
-              @tile_edges[their_id] = other_edges2 # this should be here but it breaks things
-              puts "#{their_id} is placed next to #{tile_id}"
-              found = their_id
-            end
-            # apply the operation to that tile
-            # check it fits
-          end
+      # Look in each direction
+      @directions.each do |direction|
+        try_pos = pos_direction(pos, direction)
+        # Don't look again if we already found something there
+        if (tile = @pos_to_tile[try_pos])
+          puts "- Not looking #{dir_word direction} because tile #{tile} is there."
+          next
         end
-      end
-
-      if found && !loopback_skip
-        directions_lost = 0
-        set_tile_pos(found, next_pos)
-        pos = next_pos
-        @free_tiles.delete(found)
-      else
-        break if directions_lost == 4
-        movement_directions.rotate!
-        old_direction = direction_words[direction]
-        direction = movement_directions.first
-        puts "\nCould not #{old_direction} facing tile. New direction is #{direction_words[direction]}"
-        found = true
-        directions_lost += 1
+        found_tile = bruteforce_find(our_id: tile_id, direction: direction)
+        if found_tile
+          set_tile_pos(found_tile, try_pos)
+          @tiles_to_search.delete(found_tile)
+          puts "\n- Placing #{found_tile} at #{try_pos}"
+          @positions_to_process << try_pos
+        end
       end
     end
 
 
-    [
-      @tile_pos[@min_x + 1i*@min_y], # topleft
-      @tile_pos[@max_x + 1i*@min_y], # topright
-      @tile_pos[@max_x + 1i*@max_y], # bottomright
-      @tile_pos[@min_x + 1i*@max_y], # bottomleft
+    corners = [
+      @pos_to_tile[@min_x + 1i*@min_y], # topleft
+      @pos_to_tile[@max_x + 1i*@min_y], # topright
+      @pos_to_tile[@max_x + 1i*@max_y], # bottomright
+      @pos_to_tile[@min_x + 1i*@max_y], # bottomleft
     ]
+    [corners, corners.inject(:*)]
   end
 
   def two
