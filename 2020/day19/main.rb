6c39b360196656167b34da81a26120bb90ec94be
Qais Patankar <qaisjp@gmail.com>
2020-12-20 02:10:19 +0000

    add part2

--- 2020/day19/main.rb
+++ 2020/day19/main.rb
@@ -15,13 +15,9 @@ class AoC
       key, right = line.split(": ")
       @rules[key.to_i] = right
     end
-
-    puts "#{@rules}"
-    # @data = T.let(data.map(&:to_i), T::Array[Integer])
-    # @data = T.let(data, T::Array[String])
   end
 
-  def build_regex(b)
+  def build_regex(n)
     regex = @reggies[n]
     if regex
       return regex
@@ -42,13 +38,48 @@ class AoC
     regex
   end
 
+  def build_regex_infinite(n, depth: 0)
+    regex = @reggies[n]
+    if regex
+      return regex
+    end
+    depth += 1
+    rule = @rules[n]
+
+    if rule.start_with? '"'
+      return rule[1..-2]
+    end
+
+    regex = rule.split(" | ").each_with_index.map do |pair, i|
+      # settled on a depth of 13 by trial and error (count became stable)
+      if depth > 14
+        next nil if i == 1
+      end
+      adjacents = pair.split.map {|n| build_regex_infinite(Integer(n), depth: depth)}.join("")
+      "(#{adjacents})"
+    end.compact.join("|")
+
+    regex = "(#{regex})"
+    @reggies[n] = regex
+    regex
+  end
+
   def one
     regex = Regexp.new("^"+build_regex(0)+"$")
     @input.count {|l| l =~ regex}
   end
 
   def two
-
+    # precompute safe regexes
+    @rules.each_with_index do |_, i|
+      if i == 0 || i == 8 || i == 11
+        next
+      end
+      build_regex(i)
+    end
+    puts 'precompute done'
+    regex = Regexp.new("^"+build_regex_infinite(0)+"$")
+    @input.count {|l| l =~ regex}
   end
 end
 
