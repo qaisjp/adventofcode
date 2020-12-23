c2d7144911702f84006f924d8e013bbe2d423c86
Qais Patankar <qaisjp@gmail.com>
2020-12-23 05:38:20 +0000

    part1

--- 2020/day23/main.rb
+++ 2020/day23/main.rb
@@ -9,17 +9,93 @@ class AoC
 
   def initialize(data)
     # @data = T.let(data.map(&:to_i), T::Array[Integer])
-    @data = T.let(data, T::Array[String])
+    @label = T.let(data[0].chars.map(&:to_i), T::Array[Integer])
+  end
+  
+  def get(n)
+    size = @label.size
+    @label[size % n]
   end
 
   def one
-    @data.each do |line|
-      puts "X: #{line}"
+    100.times do |move_id|
+      puts "-- move #{move_id+1} --"
+      current = @label.first
+
+      puts "cups: (#{current}) #{@label.join(" ")}"
+      a, b, c = @label.delete_at(1), @label.delete_at(1), @label.delete_at(1)
+      puts "pick up #{a}, #{b}, #{c}"
+
+      needle = current-1
+      minval = @label.min
+      found_index = nil
+      while !found_index do
+        found_index = @label.find_index(needle)
+        break if found_index
+
+        needle -= 1
+        if needle < minval
+          needle = @label.max
+        end
+      end
+
+      puts "destination: #{@label[found_index]}"
+      found_index += 1
+      @label.insert(found_index, c)
+      @label.insert(found_index, b)
+      @label.insert(found_index, a)
+
+      @label.rotate!
+      puts
     end
+    
+    while @label[0] != 1
+      @label.rotate!
+    end
+    @label[1..].join("")
   end
 
   def two
+    max = @label.max
+    ((max+1)..1000000).each do |n|
+      @label << n
+    end
+    puts @label.last
+    puts "appended"
 
+    10000000.times do |move_id|
+      # puts "-- move #{move_id+1} --"
+      current = @label.first
+
+      # puts "cups: (#{current}) #{@label.join(" ")}"
+      a, b, c = @label.delete_at(1), @label.delete_at(1), @label.delete_at(1)
+      # puts "pick up #{a}, #{b}, #{c}"
+
+      needle = current-1
+      minval = @label.min
+      found_index = nil
+      while !found_index do
+        found_index = @label.find_index(needle)
+        break if found_index
+
+        needle -= 1
+        if needle < minval
+          needle = @label.max
+        end
+      end
+
+      # puts "destination: #{@label[found_index]}"
+      found_index += 1
+      @label.insert(found_index, c)
+      @label.insert(found_index, b)
+      @label.insert(found_index, a)
+
+      @label.rotate!
+      # puts
+    end
+    
+    i = @label.find_index(1)
+    [get(i+1), get(i+2)]
   end
 end
 
