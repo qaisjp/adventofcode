91beebaf6b557cf25890f84a9d1f852f51dcba44
Qais Patankar <qaisjp@gmail.com>
2021-12-02 03:49:19 +0000

    add example to dayXX tester

--- 2021/dayXX/main.rb
+++ 2021/dayXX/main.rb
@@ -16,16 +16,50 @@ class AoC
     @data.each do |line|
       puts "X: #{line}"
     end
+
+    0
   end
 
   def two
 
+    1
+  end
+end
+
+$examples = [
+  0,
+  0,
+]
+
+def test(part)
+  runner = AoC.new File.read("example.txt").split("\n")
+
+  one = runner.one
+  if one != $examples.first
+    puts "Example part 1 was #{one}, expected #{$examples.first}"
+  else
+    puts "Example part 1 passes"
+  end
+
+  if part == "2"
+    two = runner.two
+    if two != $examples.first
+      puts "Example part 2 was #{two}, expected #{$examples[1]}"
+    else
+      puts "Example part 2 passes"
+    end
   end
 end
 
 def main
   n = ARGV.shift
-  runner = AoC.new ARGF.readlines.to_a
+  if ARGV.empty?
+    runner = AoC.new File.read("input.txt").split("\n")
+  else
+    runner = AoC.new ARGF.readlines.to_a
+  end
+
+  test(n)
 
   if n == '1'
     puts "Result: #{runner.one}"
@@ -33,4 +67,5 @@ def main
     puts "Result: #{runner.two}"
   end
 end
+
 main
