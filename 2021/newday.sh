15e00d1808d8d68b7f308b1fddde0b421f8c6b25
Qais Patankar <qaisjp@gmail.com>
2021-12-02 03:34:13 +0000

    add a new day script

---
+++ 2021/newday.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+if [ -z "$1" ]; then
+    echo "needs day number as param"
+    exit 1
+fi
+
+daystr=$(printf "day%02d" "$1")
+echo "$daystr"
+
+cp -r ../2020/dayXX $daystr
+curl -b session=$(cat token.txt) https://adventofcode.com/2021/day/$1/input > $daystr/input.txt
