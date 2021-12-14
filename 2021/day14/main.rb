1c2f9fc5951a4e41bac3ff567cb05833a9576203
Qais Patankar <qaisjp@gmail.com>
2021-12-14 14:18:21 +0000

    day14/part2

--- 2021/day14/main.rb
+++ 2021/day14/main.rb
@@ -26,10 +26,11 @@ class AoC
     end
   end
 
-  def apply(adj)
+  def apply(adj, counts)
     adj.each_with_object(Hash.new(0)) do |(pair, count), h|
       a, b = pair.chars
       if (middle = @rules["#{a}#{b}"])
+        counts[middle] += count
         h[a + middle] += count
         h[middle + b] += count
       else
@@ -38,52 +39,30 @@ class AoC
     end
   end
 
-  def one
-    puts "@rules: #{@rules}"
+  def doit(n)
     adj = str_to_adj(@template)
 
-    3.times do
-      adj = apply(adj)
+    counts = Hash.new(0).merge(@template.chars.tally)
+    n.times do |i|
+      adj = apply(adj, counts)
     end
 
-    puts
-    puts "adj:#{adj}"
-    puts
+    counts.values.max - counts.values.min
+  end
 
-    puts adj.keys.each_cons(2).to_a.inspect
-
-    counts = Hash.new(0)
-    adj.each do |pair, count|
-      a, b = pair.chars
-      counts[a] += count
-      counts[b] += count
-    end
-
-    puts "counts: #{counts}"
-
-    counts = counts.invert.keys
-    counts.max - counts.min
+  def one
+    doit(10)
   end
 
   def two
-    str = @template
-    40.times do |i|
-      puts i
-      str = apply(str)
-    end
-    # puts str
-
-    mc = str.chars.most_common
-    lc = str.chars.least_common
-
-    str.count(mc) - str.count(lc)
+    doit(40)
   end
 end
 
 def test(part)
   examples = [
     1588,
-    0,
+    2188189693529,
   ]
 
   runner = AoC.new File.read("example.txt").split("\n\n")
