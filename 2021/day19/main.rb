59f97e8558bafc6b615d4c98a1e386e52a9c62a7
Qais Patankar <qaisjp@gmail.com>
2021-12-21 00:41:06 +0000

    2021/day19/part2/example works

--- 2021/day19/main.rb
+++ 2021/day19/main.rb
@@ -3,7 +3,6 @@
 
 require 'scanf'
 require 'matrix'
-require 'set'
 
 module CoordSystem
   def x; self[0]; end
@@ -15,7 +14,15 @@ module CoordSystem
 end
 
 
-class Vector; include CoordSystem; end
+class Vector
+  attr_accessor :src_scanner
+  attr_accessor :src_coord_index
+  include CoordSystem
+
+  def manhattan
+    map(&:abs).sum
+  end
+end
 class Matrix
   include CoordSystem
 
@@ -82,17 +89,25 @@ class Array
   end
 
   OffsetsCache = {}
-  def find_each_offsets
-    OffsetsCache[self] ||= size.times.map do |i|
-      self.diffs_from_origin(i)
+  def find_each_offsets(si=nil)
+    OffsetsCache[[self,si]] ||= size.times.map do |i|
+      self.diffs_from_origin(i, si)
     end
   end
 
-  def diffs_from_origin(idx)
+  def diffs_from_origin(idx, si)
     origin = self[idx]
-    self.map do |x|
-      x - origin
-    end.to_set
+    self.each_with_index.map do |x, i|
+      vec = x - origin
+      if si
+        vec.src_scanner = si
+        vec.src_coord_index = i
+      else
+        vec.src_scanner = x.src_scanner
+        vec.src_coord_index = x.src_coord_index
+      end
+      vec
+    end
   end
 end
 
@@ -125,7 +140,20 @@ class AoC
         next
       end
     end
+
+
     @scanner_count = @scanners.size
+
+
+    @scanner_summer = {
+      0 => [],
+    }
+    @oriented_scanner_grids = {
+      0 => @scanners.first.column_vectors,
+    }
+    @spos = {
+      0 => Vector[0,0,0]
+    }
   end
 
   def try_merging_offsets(origin_all_offsets)
@@ -135,24 +163,79 @@ class AoC
     # Try to solve a scanner
     @scanners.each_with_index do |b_scanner, b_scanner_index|
       # For every possible `a_offsets` (list of offsets from a potential origin in a)
-      origin_all_offsets.each do |a_offsets|
+      origin_all_offsets.each_with_index do |a_offsets, a_of_id|
 
         # For every orientation of that scanner
-        b_scanner.orientations.each do |b_grid|
-          b_grid.find_each_offsets.each do |b_offsets|
+        b_scanner.orientations.each_with_index do |b_grid, b_or_id|
+          b_grid.find_each_offsets(b_scanner.scanner_index).each_with_index do |b_offsets, b_of_id|
+            # raise unless b_offsets[b_of_id] == Vector[0,0,0]
 
             intersections = a_offsets.intersection(b_offsets).size
 
             if intersections >= 12
-              puts "#{intersections} for scanner #{b_scanner.scanner_index} against scanner 0"
+              puts "#{intersections} for scanner #{b_scanner.scanner_index}"
 
               before = Time.now
+
               # merge b_offsets into a_offsets
-              new_offsets = (a_offsets + b_offsets).to_a.find_each_offsets
+              new_offsets = a_offsets.union(b_offsets).find_each_offsets
               puts "Took #{Time.now - before} seconds to recompute origin_all_offsets"
 
               # Don't keep trying scanners since we've solved it.
               @scanners.delete_at(b_scanner_index)
+              @oriented_scanner_grids[b_scanner.scanner_index] = b_grid
+
+              # Find a common offset in b_offsets and a_offsets.
+              a_offsets.each do |a|
+                b_offsets.each do |b|
+                  if a == b
+
+                    tree = @scanner_summer[b.src_scanner] = [a.src_scanner] + @scanner_summer.fetch(a.src_scanner)
+                    puts "scanner b:#{b.src_scanner} connected to #{a.src_scanner} — tree is #{tree}"
+                    puts
+
+                    b_pinpoint = b_grid[b.src_coord_index]
+                    puts "b_pinpoint: #{b_pinpoint}"
+
+                    a_pinpoint = @oriented_scanner_grids[a.src_scanner][a.src_coord_index]
+                    puts "a_pinpoint: #{a_pinpoint}"
+
+
+                    puts "@spos[#{b.src_scanner}] = a_pinpoint - b_pinpoint = #{a_pinpoint - b_pinpoint}"
+                    @spos[b.src_scanner] = a_pinpoint - b_pinpoint
+                    # puts "#{@spos[a.src_scanner]} - #{b_scanner.coords[b.src_coord_index]} = #{@spos[b.src_scanner].inspect}"
+                    # @sdis[b.src_scanner] = ().abs
+
+                    @oriented_scanner_offsets = @spos.map do |si, spo|
+                      things_to_sum = @scanner_summer[si]
+                      puts "things to sum #{things_to_sum}"
+                      puts "#{spo}"
+                      [spo, *things_to_sum.map {@spos[_1]}].inject(&:+)
+                    end
+
+                    puts "@spos is now #{@spos}"
+                    puts; puts
+                    completed = @scanner_count - @scanners.size
+                    left = @scanners.size
+                    percent = (completed.to_f / @scanner_count) * 100
+                    elapsed = Time.now - @start_time
+                    elapsed_minutes = (elapsed / 60).round(2)
+                    puts "PROGRESS: #{completed} of #{@scanner_count} completed, #{left} left, #{percent}% - #{elapsed} seconds elapsed (#{elapsed_minutes} minutes)"
+                    puts; puts;
+                    return new_offsets
+                  end
+                end
+              end
+
+
+              # original_a = @origin_scanner_coords[a_of_id]
+              # puts "#a_of_id is #{a_of_id.inspect} and #{original_a.inspect}"
+
+              # Given offset(of_id)
+
+              # diff = b_grid[b_of_id] - a_offsets[a_of_id]
+              # @oriented_scanner_offsets << diff
+
               return new_offsets
             end
           end
@@ -167,12 +250,12 @@ class AoC
   def one
     parse
 
-    start_time = Time.now
-    origin_scanner = @scanners.shift
+    @start_time = Time.now
+    @origin_scanner_coords = @scanners.shift.coords
 
     # there is no one origin_grid_offsets,
     # as each coordinate in the origin scanner can be an origin
-    origin_all_offsets = origin_scanner.coords.find_each_offsets
+    origin_all_offsets = @origin_scanner_coords.find_each_offsets(0)
 
     while !@scanners.empty?
       origin_all_offsets = try_merging_offsets(origin_all_offsets)
@@ -181,14 +264,17 @@ class AoC
       end
     end
 
-    puts "Taken #{Time.now - start_time} seconds to complete"
+    puts "Taken #{Time.now - @start_time} seconds to complete"
     @offsets = origin_all_offsets.first.to_a.sort3
     origin_all_offsets.first.size
   end
 
   def two
-    puts "#{@offsets.diffs_from_origin(0).to_a.sort3.last}"
-    3621
+    puts "oriented scanner offsets: #{@oriented_scanner_offsets}"
+    differences = @oriented_scanner_offsets.combination(2).map {|a, b| (b - a).manhattan}.max
+    # differences = @sdis.values.combination(2).map {|a, b| b - a}.map(&:abs).max
+    # puts "differences between each #{differences}"
+    differences
   end
 end
 
