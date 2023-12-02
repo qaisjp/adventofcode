764dd3f2e40821d9908785cf4a3b2ea660b28d1e
qaisjp <qaisjp@gmail.com>
2023-12-01 19:37:44 -0500

    Add dayXX 2023

---
+++ 2023/newday.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+if [ -z "$1" ]; then
+    echo "needs day number as param"
+    exit 1
+fi
+
+YEAR=2023
+
+daystr=$(printf "day%02d" "$1")
+echo "$daystr"
+
+cp -r dayXX $daystr
+curl -b session=$(cat token.txt) https://adventofcode.com/$YEAR/day/$1/input > $daystr/input.txt
+open "https://adventofcode.com/$YEAR/day/$1"
+code . -g "$daystr/main.rb:16"
+cd $daystr
+echo "Opening shell into new directory"
+$SHELL
