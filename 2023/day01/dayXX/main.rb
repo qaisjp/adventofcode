2b648e196a00a90dcd4e8f8ce1a341595d5b24bf
qaisjp <qaisjp@gmail.com>
2023-12-01 19:21:07 -0500

    2023/day01 part1

---
+++ 2023/day01/dayXX/main.rb
@@ -0,0 +1,86 @@
+# frozen_string_literal: true
+# typed: true
+
+require 'scanf'
+require 'set'
+
+# AoC
+class AoC
+  def initialize(data)
+    # @data = data.map(&:to_i)
+    # @data = data.first
+    @data = data
+  end
+
+  EG1 = 0
+  def one
+    @data.each do |line|
+      puts "X: #{line}"
+    end
+
+    0
+  end
+
+  EG2 = 0
+  def two
+    0
+  end
+end
+
+#####
+##### BOILERPLATE
+#####
+
+def test(_part)
+  runner = AoC.new File.read('example.txt').split("\n")
+
+  one = runner.one
+  this = AoC::EG1
+  if one != this
+    puts "Example part 1 was #{one}, expected #{this}"
+  else
+    puts "Example part 1 passes (#{one})"
+    passed = true
+  end
+
+  if passed
+    puts "\n\n--- Example part 2"
+    two = runner.two
+    this = AoC::EG2
+    if two != this
+      puts "\n\nExample part 2 was #{two}, expected #{this}"
+      passed = false
+    else
+      puts "\n\nExample part 2 passes (#{this})"
+    end
+  end
+
+  passed
+end
+
+def main
+  run_both = false
+
+  n = ARGV.shift
+
+  if !test(n)
+    puts "Tests failed :("
+    return
+  end
+
+  if ARGV.empty?
+    runner = AoC.new File.read("input.txt").split("\n")
+  else
+    runner = AoC.new ARGF.readlines.to_a
+  end
+
+  puts "\n\n--- Part #{n} solution" if n != nil
+  if n == '1' || (run_both && n == '2')
+    puts "Result 1: #{runner.one}"
+  end
+  if n == '2'
+    puts "Result 2: #{runner.two}"
+  end
+end
+
+main
---
+++ 2023/day01/main.rb
@@ -0,0 +1,89 @@
+# frozen_string_literal: true
+# typed: true
+
+require 'scanf'
+require 'set'
+
+# AoC
+class AoC
+  def initialize(data)
+    # @data = data.map(&:to_i)
+    # @data = data.first
+    @data = data
+  end
+
+  EG1 = 142
+  def one
+    @data.sum do |line|
+      nums = line.chars.filter {|c| c.match?(/[0-9]/) }.map(&:to_i)
+      puts("line is #{line} splitted is #{nums}")
+      "#{nums[0]}#{nums[-1]}".to_i
+    end
+  end
+
+  EG2 = 0
+  def two
+    @data.sum do |line|
+      num_from_line(line)
+    end
+  end
+end
+
+#####
+##### BOILERPLATE
+#####
+
+def test(_part)
+  runner = AoC.new File.read('example.txt').split("\n")
+
+  one = runner.one
+  this = AoC::EG1
+  if one != this
+    puts "Example part 1 was #{one}, expected #{this}"
+  else
+    puts "Example part 1 passes (#{one})"
+    passed = true
+  end
+
+  if passed
+    runner = AoC.new File.read('example2.txt').split("\n")
+    puts "\n\n--- Example part 2"
+    two = runner.two
+    this = AoC::EG2
+    if two != this
+      puts "\n\nExample part 2 was #{two}, expected #{this}"
+      passed = false
+    else
+      puts "\n\nExample part 2 passes (#{this})"
+    end
+  end
+
+  passed
+end
+
+def main
+  run_both = false
+
+  n = ARGV.shift
+
+  if !test(n)
+    puts "Tests failed :("
+    return
+  end
+
+  if ARGV.empty?
+    runner = AoC.new File.read("input.txt").split("\n")
+  else
+    runner = AoC.new ARGF.readlines.to_a
+  end
+
+  puts "\n\n--- Part #{n} solution" if n != nil
+  if n == '1' || (run_both && n == '2')
+    puts "Result 1: #{runner.one}"
+  end
+  if n == '2'
+    puts "Result 2: #{runner.two}"
+  end
+end
+
+main
