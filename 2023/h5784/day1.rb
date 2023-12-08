a24782eb26eaa150f0168c5d9b2c4be79173369c
qaisjp <qaisjp@gmail.com>
2023-12-08 02:28:26 -0500

    h5784/day1

---
+++ 2023/h5784/day1.rb
@@ -0,0 +1,46 @@
+# typed: true
+
+require 'json'
+require 'scanf'
+require 'set'
+require 'sorbet-runtime'
+require 'benchmark'
+require 'pry'
+
+class H5784
+    Mapping = T.let({
+        0 => [' '],
+        1 => [],
+        2 => "ABC".chars,
+        3 => "DEF".chars,
+        4 => "GHI".chars,
+        5 => "JKL".chars,
+        6 => "MNO".chars,
+        7 => "PQRS".chars,
+        8 => "TUV".chars,
+        9 => "WXYZ".chars
+    }, T::Hash[Integer, T::Array[String]])
+
+    def main
+        File.read("./data/noahs-customers.jsonl").split("\n").each do |line|
+            JSON.parse(line, symbolize_names: true) => {name:, phone:}
+            o_phone = phone
+            phone = phone.tr("-", "")
+            name.upcase!
+
+            surname = name.split(" ")[1]
+            if phone.size == surname.size
+                thingy = surname.chars.each_with_index.count do |c, index|
+                    digit = phone.chars.fetch(index).to_i
+                    Mapping.fetch(digit).include? c
+                end
+            end
+
+            if thingy == surname.size
+                puts({name:, surname:, o_phone:}.to_json)
+            end
+        end
+    end
+end
+
+H5784.new.main
