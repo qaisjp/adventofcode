7c94bfcd3651522d08953a220d822ac7483f9de6
qaisjp <qaisjp@gmail.com>
2023-12-09 00:10:59 -0500

    day09 part1

---
+++ 2023/day09/main.rb
@@ -0,0 +1,108 @@
+# frozen_string_literal: true
+# typed: true
+
+require 'scanf'
+require 'set'
+require 'sorbet-runtime'
+# AoC
+class AoC
+  extend T::Sig
+  def initialize(data)
+    # @data = T.let(data.map(&:to_i), T::Array[Integer])
+    # @data = T.let(data.first, String)
+    @data = T.let(data, T::Array[String])
+  end
+
+  def recursive_next(arr)
+    all_zero = T.let(true, T::Boolean)
+    nums = arr.each_cons(2).map do |a, b|
+      n = b - a
+      # if n < 0
+      #   raise "wtf #{n}"
+      # end
+      if n != 0
+        all_zero = false
+      end
+      n
+    end
+
+    if all_zero
+      return 0
+    end
+    return nums.last + recursive_next(nums)
+  end
+
+  EG1 = 114
+  def one
+    @data.map do |line|
+      nums = line.split.map(&:to_i)
+      puts("Processing #{line}")
+      result = T.must(nums.last) + recursive_next(nums)
+      puts("Result was #{result}")
+      result
+    end.sum
+  end
+
+  EG2 = 0
+  def two
+    0
+  end
+end
+
+#####
+##### BOILERPLATE
+#####
+
+def test(_part)
+  runner = AoC.new File.read('example.txt').split("\n")
+
+  one = runner.one
+  this = AoC::EG1
+  if one != this
+    puts "Example part 1 was #{one}, expected #{this}"
+  else
+    puts "Example part 1 passes (#{one})"
+    passed = true
+  end
+
+  if passed
+    puts "\n\n--- Example part 2"
+    two = runner.two
+    this = AoC::EG2
+    if two != this
+      puts "\n\nExample part 2 was #{two}, expected #{this}"
+      passed = false
+    else
+      puts "\n\nExample part 2 passes (#{this})"
+    end
+  end
+
+  passed
+end
+
+def main
+  run_both = T.let(false, T::Boolean)
+
+  n = ARGV.shift
+
+  if !test(n)
+    puts "Tests failed :("
+    return
+  end
+
+  if ARGV.empty?
+    runner = AoC.new File.read("input.txt").split("\n")
+  else
+    runner = AoC.new ARGF.readlines.to_a
+  end
+
+  puts "\n\n--- Part #{n} solution" if n != nil
+  if n == '1' || (run_both && n == '2')
+    puts "Result 1: #{runner.one}"
+  end
+  if n == '2'
+    puts "Result 2: #{runner.two}"
+  end
+end
+
+main
