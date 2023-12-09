5718e02b0afe04c1778271b3780819c460ec08d2
qaisjp <qaisjp@gmail.com>
2023-12-09 00:38:00 -0500

    h784/day2

---
+++ 2023/h5784/day2.sql
@@ -0,0 +1,16 @@
+select
+    c.name, c.phone, o.customerid, o.ordered, o.shipped
+from
+    orders o, customers c, orders_items oi
+where
+    -- in 2017
+    o.ordered like '2017%' and
+
+    -- get the initials
+    o.customerid = c.customerid and
+    substr(c.name, 1, 1) = 'J' and
+    substr(c.name, instr(c.name, ' ')+1, 1) = 'P'
+
+    -- for the rug cleaner sku (sku found by cmd-fing noahs-orders_items.csv)
+    and oi.orderid = o.orderid
+    and sku = 'HOM2761';
